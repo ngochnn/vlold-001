4d8bfb38c47d881e9a49098a782c92c555856413
ngochnn <49011220+ngochnn@users.noreply.github.com>
2020-04-09 10:34:51 +0700

    Update vd01.py

--- vd01.py
+++ vd01.py
@@ -1,29 +1,36 @@
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Giải phương trình bậc hai</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <?php 
-            $result = '';
-            if (isset($_POST['calculate']))
-            {
-            }
-        ?>
-        <h1>Giải phương trình bậc hai</h1>
-        <form method="post" action="">
-            <input type="text" style="width: 20px" name="a" value=""/>x <sup>2</sup>
-            +
-            <input type="text" style="width: 20px" name="b" value=""/>x
-            + 
-            <input type="text" style="width: 20px" name="c" value=""/>
-            = 0
-            <br/><br/>
-            <input type="submit" name="calculate" value="Tính" />
-        </form>
-        <?php echo $result; ?>
-    </body>
-</html>
+import math
+ 
+"""
+# Giải phương trình bậc 2: ax2 + bx + c = 0
+# @param a: hệ số bậc 2
+# @param b: hệ số bậc 1
+# @param c: số hạng tự do
+"""
+def giaiPTBac2(a, b, c):
+    # kiểm tra các hệ số
+    if (a == 0):
+        if (b == 0):
+            print ("Phương trình vô nghiệm!");
+        else:
+            print ("Phương trình có một nghiệm: x = ", + (-c / b));
+        return;
+ 
+    # tính delta
+    delta = b * b - 4 * a * c;
+    # tính nghiệm
+    if (delta > 0):
+        x1 = (float)((-b + math.sqrt(delta)) / (2 * a));
+        x2 = (float)((-b - math.sqrt(delta)) / (2 * a));
+        print ("Phương trình có 2 nghiệm là: x1 = ", x1, " và x2 = ", x2);
+    elif (delta == 0):
+        x1 = (-b / (2 * a));
+        print("Phương trình có nghiệm kép: x1 = x2 = ", x1);
+    else:
+        print("Phương trình vô nghiệm!");
+ 
+# Nhập các hệ số
+a = float(input("Nhập hệ số bậc 2, a = "));
+b = float(input("Nhập hệ số bậc 1, b = "));
+c = float(input("Nhập hằng số tự do, c = "));
+# Gọi hàm giải phương trình bậc 2
+giaiPTBac2(a, b, c)
